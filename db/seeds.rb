84a8c3345564ed967873f4b05588bf2b6729a979
Ari Brenner <aribrenner@gmail.com>
2017-08-31 16:06:53 -0400

    add brand_id to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,17 +6,17 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Snack.create!(name: 'Nice! Snack Mix', calories: 100)
-Snack.create!(name: 'Nice! Rocky Road Ice Cream', calories: 300)
 
-Snack.create!(name: 'Microwave Pizza', calories: 400)
+mars = Brand.create!(name: 'Mars', logo: 'https://i.ytimg.com/vi/FW0kUmoTQFM/maxresdefault.jpg')
+digiorno = Brand.create!(name: 'DiGiorno', logo: 'http://www.digiorno.com/Content/Images/header/DiGiornoLogo.png')
+duane_read = Brand.create!(name: 'Duane Reade', logo: 'https://upload.wikimedia.org/wikipedia/en/thumb/0/06/Duane_Reade_Logo.svg/849px-Duane_Reade_Logo.svg.png')
+ari_co = Brand.create!(name: 'Ari Co', logo: 'http://aribrenner.com/media/images/ari0.jpg')
 
-Snack.create!(name: 'M&Ms', calories: 300)
-Snack.create!(name: 'Skittles', calories: 300)
-Snack.create!(name: 'Snickers', calories: 300)
+Snack.create!(name: 'Nice! Snack Mix', calories: 100, brand_id: duane_read.id)
+Snack.create!(name: 'Nice! Rocky Road Ice Cream', calories: 300, brand_id: duane_read.id)
 
+Snack.create!(name: 'Microwave Pizza', calories: 400, brand_id: digiorno.id)
 
-Brand.create!(name: 'Mars', logo: 'https://i.ytimg.com/vi/FW0kUmoTQFM/maxresdefault.jpg')
-Brand.create!(name: 'DiGiorno', 'http://www.digiorno.com/Content/Images/header/DiGiornoLogo.png')
-Brand.create!(name: 'Duane Reade', logo: 'https://upload.wikimedia.org/wikipedia/en/thumb/0/06/Duane_Reade_Logo.svg/849px-Duane_Reade_Logo.svg.png')
-Brand.create!(name: 'Ari Co', logo: 'http://aribrenner.com/media/images/ari0.jpg')
+Snack.create!(name: 'M&Ms', calories: 300, brand_id: mars.id)
+Snack.create!(name: 'Skittles', calories: 300, brand_id: mars.id)
+Snack.create!(name: 'Snickers', calories: 300, brand_id: mars.id)
