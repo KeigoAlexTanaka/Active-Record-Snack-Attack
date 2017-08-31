1b6d86e3baee2975a4f24bec2b067c09102c34fe
Ari Brenner <aribrenner@gmail.com>
2017-08-31 15:41:53 -0400

    seed brands

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,4 +16,7 @@ Snack.create!(name: 'Skittles', calories: 300)
 Snack.create!(name: 'Snickers', calories: 300)
 
 
-Brand.create!(name: 'Mars', )
+Brand.create!(name: 'Mars', logo: 'https://i.ytimg.com/vi/FW0kUmoTQFM/maxresdefault.jpg')
+Brand.create!(name: 'DiGiorno', 'http://www.digiorno.com/Content/Images/header/DiGiornoLogo.png')
+Brand.create!(name: 'Duane Reade', logo: 'https://upload.wikimedia.org/wikipedia/en/thumb/0/06/Duane_Reade_Logo.svg/849px-Duane_Reade_Logo.svg.png')
+Brand.create!(name: 'Ari Co', logo: 'http://aribrenner.com/media/images/ari0.jpg')
